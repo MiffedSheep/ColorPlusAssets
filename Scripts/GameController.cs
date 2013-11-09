644af39d35417af6d748736ea9801aa75f68170a
MiffedSheep(MattG) <mwg13@hampshire.edu>
2013-11-09 15:32:43 -0500

    Basic Script
    This just instantiates an 8 x 5 grid and tracks the location of the
    cubes on each cube to be later referenced by the OnMouseDown() and
    GetKeyInput() methods.

---
+++ Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+public int gridWidth = 8;
+public int gridHeight =5;
+public GameObject aCube;
+public GameObject[,] cubes;
+	// Use this for initialization
+	void Start () {
+	cubes = new GameObject [gridWidth, gridHeight];
+		
+	for(int x = 0; x<gridWidth; x++){
+		for(int y = 0; y<gridHeight;y++){
+		cubes [x, y] = (GameObject)Instantiate (aCube, new Vector3 (x * 2 - 14, y * 2 - 8, 10), Quaternion.identity);		
+		cubes [x, y].GetComponent<CubeBehavior> ().x = x;
+		cubes [x, y].GetComponent<CubeBehavior> ().y = y;		
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
