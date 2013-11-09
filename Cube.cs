f94d9f174ee47a5868b929886234120ad52ba628
MiffedSheep(MattG) <mwg13@hampshire.edu>
2013-11-09 15:18:54 -0500

    Basic Work
    Okay, so this set up instantia

---
+++ Cube.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube {
+	
+	public int x,y;
+	public int currentX,currentY;
+	public bool Black, Blue, Green, Red, Yellow;
+	public GameObject aCube;
+	// Use this for initialization
+	public Cube(){
+		
+	}
+	
+	public Cube(bool Black, bool Blue, bool Green, bool Red, bool Yellow, int x, int y){
+		Black = false;
+		Blue=false;
+		Green=false;
+		Red=false;
+		Yellow=false;
+		x=currentX;
+		y=currentY;
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GameControllerScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControllerScript : MonoBehaviour {
+	public int gridWidth = 8;
+	public int gridHeight= 5;
+	public bool Black, Blue, Green, Red, Yellow;
+	public GameObject aCube;
+	public GameObject[,] cubes;
+	private Cube cube;
+	private CubeBehavior cubeBehavior;
+	public string kubeLogic;
+	// Use this for initialization
+	void Start () {
+	cubes = new GameObject[gridWidth,gridHeight];
+	//cube = new Cube(!Black,!Blue,!Green,!Red,!Yellow,0,0);
+	cubeBehavior = aCube.GetComponent<CubeBehavior>();	
+	
+	for(int x =0; x <gridWidth; x++){
+		for(int y=0; y<gridHeight; y++){	
+			cubes [x,y] = (GameObject)Instantiate(aCube, new Vector3 (x*2-14, y*2-8,0), Quaternion.identity);		
+			cubes [x,y].GetComponent<CubeBehavior>().x = x;
+			cubes [x,y].GetComponent<CubeBehavior>().y = y;
+			//-6.204506,-3.67789,-10.02057
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnMouseDown(){
+	cube = new Cube(!Black,!Blue,!Green,!Red,!Yellow,0,0);	
+	}
+}
